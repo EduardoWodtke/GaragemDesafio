5c0b68170f6f620b8231f7b615d924c327d76a46
EduardoWodtke <eduardorwodtkeifc@gmail.com>
2024-03-22 16:42:51 -0300

    Criação da api da Cor/Acessorio

--- Garagem/admin.py
+++ Garagem/admin.py
@@ -3,8 +3,12 @@ from django.contrib import admin
 from .models import Categoria
 from .models import Marca
 from .models import Modelo
+from .models import Cor
+from .models import Acessorio
 
 
 admin.site.register(Categoria)
 admin.site.register(Marca)
 admin.site.register(Modelo)
+admin.site.register(Cor)
+admin.site.register(Acessorio)
--- Garagem/models.py
+++ Garagem/models.py
@@ -10,3 +10,10 @@ class Modelo(models.Model):
     nome = models.CharField(max_length=50)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="Modelo", null=False, blank=False)
     marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="Modelo", null=True, blank=False)
+
+class Cor(models.Model):
+    nome = models.CharField(max_length=20)
+
+class Acessorio(models.Model):
+    descricao = models.CharField(max_length=50)
+
