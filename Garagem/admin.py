fda02ac2f66f3bd2cbd42417a236292d15225ef2
EduardoWodtke <eduardorwodtkeifc@gmail.com>
2024-03-22 16:32:12 -0300

    Criação da api da Marca/Modelo

--- Garagem/admin.py
+++ Garagem/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Categoria
+from .models import Marca
+from .models import Modelo
+
+
+admin.site.register(Categoria)
+admin.site.register(Marca)
+admin.site.register(Modelo)
--- Garagem/models.py
+++ Garagem/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
-class categoria(models.Model):
+class Categoria(models.Model):
     descricao = models.CharField(max_length=50)
+
+class Marca(models.Model):
+    nome = models.CharField(max_length=50)
+
+class Modelo(models.Model):
+    nome = models.CharField(max_length=50)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="Modelo", null=False, blank=False)
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="Modelo", null=True, blank=False)
